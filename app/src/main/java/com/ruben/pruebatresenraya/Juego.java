35bcc0c74339b730a757d9b16c680a918e432c5d
ruben lopez <socoruben@gmail.com>
2019-11-03 18:26:54 +0100

    Primer commit

---
+++ app/src/main/java/com/ruben/pruebatresenraya/Juego.java
@@ -0,0 +1,14 @@
+package com.ruben.pruebatresenraya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Juego extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.juego_vista);
+    }
+}
---
+++ app/src/main/java/com/ruben/pruebatresenraya/Principal.java
@@ -0,0 +1,20 @@
+package com.ruben.pruebatresenraya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Principal extends AppCompatActivity {
+
+    private EditText etRegistro;
+    private Button bRegistro;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_vista);
+
+
+    }
+}
