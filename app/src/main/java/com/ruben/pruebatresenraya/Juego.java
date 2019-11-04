f1666c90170470e335647c1653b3a43be39c5d98
ruben lopez <socoruben@gmail.com>
2019-11-04 13:13:18 +0100

    he vuelto a cambiar el boton

--- app/src/main/java/com/ruben/pruebatresenraya/Juego.java
+++ app/src/main/java/com/ruben/pruebatresenraya/Juego.java
@@ -9,6 +9,6 @@ public class Juego extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.juego_vista);
+        setContentView(R.layout.juego_vista)t;
     }
 }
