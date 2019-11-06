f3adbeef1949db22dd7d80c41746f4d674fc901f
ruben lopez <socoruben@gmail.com>
2019-11-06 18:40:59 +0100

    metodos para verificar el radioButton y e iniciar

--- app/src/main/java/com/ruben/pruebatresenraya/Principal.java
+++ app/src/main/java/com/ruben/pruebatresenraya/Principal.java
@@ -2,10 +2,13 @@ package com.ruben.pruebatresenraya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
 
@@ -30,7 +33,40 @@ public class Principal extends AppCompatActivity {
         rbX2 = (RadioButton) findViewById(R.id.rbX2);
     }
 
+    /* metodos  para cambiar el radioButton si los dos jugadores elijen el mismo tipo. */
+    public void verifChekedO(View view){
+        if (rbO1.isChecked() == rbO2.isChecked() && (rbO1.isChecked()|| rbO2.isChecked())){
+            rbX1.setChecked(true);
+        }
+
+    }
+
+    public void verifChekedX(View view)
+    {
+        if (rbX1.isChecked() == rbX2.isChecked() && (rbX1.isChecked()|| rbX2.isChecked())){
+            rbO1.setChecked(true);
+        }
+    }
+
+
+    //metodo que inicia la actividad del juego.
+    public void iniciar(View view) {
+        if (etNombre1.getText().toString().isEmpty()&& etNombre2.getText().toString().isEmpty()) {
+            Toast t = Toast.makeText(this, "Se tiene que colocar nombre a los dos jugadores", Toast.LENGTH_LONG);
+            t.show();
+
+        }else{
+            Intent intent = new Intent(this,Juego.class);
+            String nombre1 = etNombre1.getText().toString();
+            String nombre2 = etNombre2.getText().toString();
+
+            intent.putExtra("jugador1",nombre1);
+            intent.putExtra("jugador2",nombre2);
+            startActivity(intent);
+
+        }
 
 
 
+    }
 }
