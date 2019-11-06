f22fa669c82d253b8d111c80d65ae254ebec2a75
daniel <danielsalasvalle@gmail.com>
2019-11-06 09:36:27 +0100

    Flaco83 yo, personalmente, he realizado cambios en la clase Principal y he creado la clase jugador

--- app/src/main/java/com/ruben/pruebatresenraya/Principal.java
+++ app/src/main/java/com/ruben/pruebatresenraya/Principal.java
@@ -5,16 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class Principal extends AppCompatActivity {
 
-    private EditText etRegistro;
-    private Button bRegistro;
+    private EditText etNombre1;
+    private EditText etNombre2;
+    private Button bJugar;
+    private RadioButton rbO1;
+    private RadioButton rbX1;
+    private RadioButton rbO2;
+    private RadioButton rbX2;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal_vista);
-
-
+        etNombre1 = (EditText) findViewById(R.id.etNombre1);
+        etNombre2 = (EditText) findViewById(R.id.etNombre2);
+        bJugar = (Button) findViewById(R.id.bJugar);
+        rbO1 = (RadioButton) findViewById(R.id.rbO1);
+        rbX1 = (RadioButton) findViewById(R.id.rbX1);
+        rbO2 = (RadioButton) findViewById(R.id.rbO2);
+        rbX2 = (RadioButton) findViewById(R.id.rbX2);
     }
+
+
+
+
 }
